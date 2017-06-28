dbee5552cf88619eabb6ab5afba5b0066588e5de
cgathergood <calumgathergood@gmail.com>
2017-06-28 15:36:49 +1000

    Initial Commit

---
+++ Mapbox/Assets/Control.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Control : MonoBehaviour {
+
+	private float rotationRate = 10.0f;
+	private float h;
+	private float v;
+
+	void Update (){
+		// get the user input
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		if (h != 0 || v != 0){
+			transform.Rotate (v * rotationRate, -h * rotationRate, 0, Space.World);
+		}
+	}
+}
---
+++ Mapbox/MapboxAR/Classes/Native/AssemblyU2DCSharp_Control2401058773.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+
+#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// Control
+struct  Control_t2401058773  : public MonoBehaviour_t1158329972
+{
+public:
+	// System.Single Control::rotationRate
+	float ___rotationRate_2;
+	// System.Single Control::h
+	float ___h_3;
+	// System.Single Control::v
+	float ___v_4;
+
+public:
+	inline static int32_t get_offset_of_rotationRate_2() { return static_cast<int32_t>(offsetof(Control_t2401058773, ___rotationRate_2)); }
+	inline float get_rotationRate_2() const { return ___rotationRate_2; }
+	inline float* get_address_of_rotationRate_2() { return &___rotationRate_2; }
+	inline void set_rotationRate_2(float value)
+	{
+		___rotationRate_2 = value;
+	}
+
+	inline static int32_t get_offset_of_h_3() { return static_cast<int32_t>(offsetof(Control_t2401058773, ___h_3)); }
+	inline float get_h_3() const { return ___h_3; }
+	inline float* get_address_of_h_3() { return &___h_3; }
+	inline void set_h_3(float value)
+	{
+		___h_3 = value;
+	}
+
+	inline static int32_t get_offset_of_v_4() { return static_cast<int32_t>(offsetof(Control_t2401058773, ___v_4)); }
+	inline float get_v_4() const { return ___v_4; }
+	inline float* get_address_of_v_4() { return &___v_4; }
+	inline void set_v_4(float value)
+	{
+		___v_4 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
