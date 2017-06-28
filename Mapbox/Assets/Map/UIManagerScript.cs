9f292acb01a9972d0f458ab34a286ef5331f7d95
cgathergood <calumgathergood@gmail.com>
2017-06-28 17:07:20 +1000

    Created basic UI

---
+++ Mapbox/Assets/Map/UIManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour {
+
+	void onStart() {
+
+	}
+
+	public void StartMap() 
+	{
+		SceneManager.LoadScene("MapDemo");
+	}
+
+	public void StartMenu() 
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Exit() 
+	{
+		Application.Quit ();
+	}
+}
